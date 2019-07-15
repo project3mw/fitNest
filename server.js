75e7290a65102ad4d388ee6452c88fc4951af78a
AP <ap@APs-MacBook-Pro.local>
2019-07-15 15:02:00 -0700

    boiler plate

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const { join } = require('path')
+const app = express()
+
+app.use(express.static(join(__dirname, 'client', 'build')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+require('./routes')(app)
+
+// require('./data')
+
+require('./config').sync()
+  .then(_ => app.listen(process.env.PORT || 3001))
+  .catch(e => console.log(e))
