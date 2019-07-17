cdbcc1a4b65575fae6b3b7e3b2b4ae59e2afc29f
GunnyD <45931516+DeanJG@users.noreply.github.com>
2019-07-17 16:46:02 -0700

    basic workout models and example routes

---
+++ models/Workout.js
@@ -0,0 +1,25 @@
+module.exports = (Schema, model) => {
+  const Workout = new Schema({
+      name: String, 
+      group: String,
+      intensity: {
+        type: Number,
+        min: 1,
+        max: 5
+      }, 
+      time: String,
+      favorite: Boolean,
+      creator: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      },
+      description: {
+        type: String,
+        match: /[a-z]/
+      }
+  })
+
+  // User.plugin(require('passport-local-mongoose'))
+
+  return model('Workout', Workout)
+}
