17f92dca51a5ba0d8a924f90f0027f5a44089d52
JagatheswariManoharan <jagatheswarimanoharan@dhcp-v097-019.mobile.uci.edu>
2019-07-17 21:30:34 -0700

    implemented passport in server.js

--- server.js
+++ server.js
@@ -1,18 +1,42 @@
 const express = require('express')
 const { join } = require('path')
+const passport = require('passport')
+const{ Strategy } = require('passport-local')
+const { Strategy: JWTStrategy,ExtractJwt } = require('passport-jwt')
+const {User} = require('./models')
 const app = express()
 
 app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+app.use(require('express-session')({
+  secret: 'fitnest' ,
+  resave: false,
+  saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.use(new Strategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+passport.use(new JWTStrategy({
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: 'fitnest'
+}, (jwtPayload, cb) => User.findById(jwtPayload.id)
+  .then(user => cb(null, user))
+  .catch(err => cb(err, null))
+))
+
 require('./routes')(app)
 
-require('mongoose').connect('mongodb://localhost/fitNestgit', { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true })
- .then(_ => {
-   console.log("database connected")
-   app.listen(3001, () => console.log('server listening on port: 3001'))
- })
- .catch(function(err) {
- console.log(err.message);
- });
+require('mongoose').connect('mongodb://localhost/fitNest', { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true })
+  .then(_ => {
+    console.log("database connected")
+    app.listen(3001, () => console.log('server listening on port: 3001'))
+  })
+  .catch(function(err) {
+  console.log(err.message);
+  });
