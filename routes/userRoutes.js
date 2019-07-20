5b62f39acf14301975640f0660e550f7fe67d5be
JagatheswariManoharan <jagatheswarimanoharan@Viswanathans-MacBook-Pro.local>
2019-07-19 23:46:56 -0700

    done passport authentication,all routes working good

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,39 @@
 const { User } = require('../models')
+const jwt = require('jsonwebtoken')
+const passport = require('passport')
 
 module.exports = app => {
+  app.post('/verify',passport.authenticate('jwt', {session: false }),(req,res) => res.sendStatus(200))
+  app.post('/register',(req,res) => {
+    User.register(new User({
+      name:req.body.name,
+      username:req.body.username,
+      email:req.body.email,
+      age:req.body.age,
+    }), req.body.password,e => {
+      if(e) throw e
+      User.authenticate()(req.body.username,
+      req.body.password, (e,user) => {
+      if (e) throw e
+      res.json({ isLoggedIn: !!user,user: user.username,
+      token: jwt.sign({ id: user._id},'fitnest')
+      })
+      })
+    })
+    // }),req.body.password, (e,user)=> {
+      // if (e) console.log(e)
+      // res.json(user)
+    // })
+  })
+  app.post('/login',(req,res) => {
+    User.authenticate()(req.body.username,
+      req.body.password, (e,user) => {
+      if (e) throw e
+      res.json({ isLoggedIn: !!user,user: user.username,
+      token: jwt.sign({ id: user._id},'fitnest')
+      })
+      })
+  })
   app.get('/users', (req, res) => {
     User.find({})
       .then(users => res.json(users))
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const { join } = require('path')
 const passport = require('passport')
 const{ Strategy } = require('passport-local')
 const { Strategy: JWTStrategy,ExtractJwt } = require('passport-jwt')
+const session = require('express-session')
 const {User} = require('./models')
 const app = express()
 require("dotenv").config()
@@ -12,14 +13,20 @@ app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.use(require('express-session')({
+
+// app.use(require('express-session')({
+//   secret: 'fitnest' ,
+//   resave: false,
+//   saveUninitialized: false
+// }))
+app.use(session({
   secret: 'fitnest' ,
   resave: false,
   saveUninitialized: false
 }))
-
 app.use(passport.initialize())
 app.use(passport.session())
+
 
 passport.use(new Strategy(User.authenticate()))
 passport.serializeUser(User.serializeUser())
@@ -34,11 +41,11 @@ passport.use(new JWTStrategy({
 
 require('./routes')(app)
 
-require('mongoose').connect(`${process.env.MONGODB_URI}`, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true })
- .then(_ => {
-   console.log("database connected")
-   app.listen(3001, () => console.log('server listening on port: 3001'))
- })
- .catch(function(err) {
- console.log(err.message);
- });
+require('mongoose').connect('mongodb://localhost:27017/auth_db', { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true })
+  .then(_ => {
+    console.log("database connected")
+    app.listen(3001, () => console.log('server listening on port: 3001'))
+  })
+  .catch(function(err) {
+  console.log(err.message);
+  });
