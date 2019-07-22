6f37ee6ec7c9028785f9b0a0df9abfd5d79410cd
Adrian Perez <adrian.perez87@gmail.com>
2019-07-22 12:28:12 -0700

    added topnav and styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-// import Login from './pages/Login'
+import Login from './pages/Login'
 // import WorkoutPlanList from './pages/WorkoutPlanList'
 import Profile from './pages/Profile'
 // import NestList from './pages/NestList'
@@ -8,12 +8,16 @@ import Profile from './pages/Profile'
 // import Create from './pages/Create'
 import ProfileTop from './components/ProfileTop'
 import NavBar from './components/Navbar'
-// import TopNav from './components/topnav'
-
-// import NB from './components/NB'
-// import TopNav from './components/topnav'
+import TopNav from './components/TopNav'
 import Stacks from './utils/Stacks.js'
+import './App.css';
+// class App extends Component {
 
+//   render() {
+//     const backStyle = style({
+//       background: 'black'
+//     })
+  
 class App extends Component {
   render () {
     Stacks.getStacks()
@@ -24,7 +28,7 @@ class App extends Component {
       
       <Router>
         <div>
-          {/* <TopNav /> */}
+          <TopNav />
           {/* <Route exact path='/' component={Login} />
           <Route path='/Create' component={Create} />
           <Route path='/NestList' component={NestList} />
@@ -32,9 +36,10 @@ class App extends Component {
           <Route path='/WorkoutPlanList' component={WorkoutPlanList} /> */}
           <Route exact path='/Profile' component={_ => <ProfileTop />} />
           <ProfileTop />
-          {/* <NB /> */}
           <NavBar />
+          
         </div>
+          
       </Router>
 
     )
