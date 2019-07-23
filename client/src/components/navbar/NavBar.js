39ccbeb20cccd042abee026f8624a4104eafa333
Adrian Perez <adrian.perez87@gmail.com>
2019-07-23 14:27:13 -0700

    chaanges to navbar

--- client/src/components/navbar/NavBar.js
+++ client/src/components/navbar/NavBar.js
@@ -1,29 +1,43 @@
 import React from "react";
+import { Link } from 'react-router-dom'
+;
 import { makeStyles } from "@material-ui/core/styles";
 import BottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
-import AccountCircleIcon from "@material-ui/icons/AccountCircle";
-import BubbleChartIcon from "@material-ui/icons/BubbleChart";
-import ListAltIcon from "@material-ui/icons/ListAlt";
-import PersonAddIcon from "@material-ui/icons/PersonAdd";
-import AddIcon from "@material-ui/icons/Add";
-import Fab from '@material-ui/core/Fab';
+import PersonIcon from "@material-ui/icons/Person";
+// import BubbleChartIcon from "@material-ui/icons/BubbleChart";
+import ListIcon from "@material-ui/icons/List";
+// import PersonAddIcon from "@material-ui/icons/PersonAdd";
+// import AddIcon from "@material-ui/icons/Add";
+// import Fab from '@material-ui/core/Fab';
+import MeetingRoomIcon from '@material-ui/icons/MeetingRoom'
+import RowingIcon from '@material-ui/icons/Rowing'
 
 const useStyles = makeStyles({
   nb: {
     width: '100%',
-    backgroundColor: 'transparent',
-    //backgroundColor: '#990000',
+    // backgroundColor: '#FF8C00',
+//     backgroundColor: '#ea2027',
+// backgroundImage: 'linear-gradient(315deg, #ea2027 0%, #ee5a24 74%)',
+backgroundColor: '#ffd045',
+backgroundImage: 'linear-gradient(315deg, #ffd045 0%, #d14545 74%)',
+// backgroundColor: '#ffcb43',
+// backgroundImage: 'linear-gradient(319deg, #ffcb43 0%, #ff6425 37%, #ff0016 100%)',
+
+
+    // backgroundColor: 'transparent',
+    // backgroundColor: '#990000',
     // backgroundImage: 'linear-gradient(147deg, #990000 0%, #ff0000 74%)',
     // backgroundColor: '#f39f86',
     // backgroundImage: 'linear-gradient(315deg, #f39f86 0%, #f9d976 74%)',
+    
     // backgroundColor: '#4B0082',
     // backgroundImage: 'linear-gradient(316deg, #6c33a3 0%, #8241b8 74%)',
     color: 'white',
     position: 'fixed',
     bottom: 0,
-    borderTopLeftRadius: '25px',
-    borderTopRightRadius: '25px',
+    borderTopLeftRadius: '30px',
+    borderTopRightRadius: '30px',
     
   },
   fabButton: {
@@ -33,10 +47,10 @@ const useStyles = makeStyles({
     left: 0,
     right: 0,
     margin: '0 auto',
-    backgroundColor: 'red',
+    backgroundColor: '#FFE6D0',
     // backgroundColor: '#990000',
     // backgroundImage: 'linear-gradient(147deg, #990000 0%, #ff0000 74%)',
-    color: 'white',
+    color: 'grey',
   },
 });
 
@@ -59,35 +73,46 @@ function NavBar() {
       className={classes.nb}
       
     >
+      <Link to='/Profile'>
       <BottomNavigationAction
-        label="Profile" style={{color: 'white'}}
-        icon={<AccountCircleIcon />}
+        label="Profile" 
+        style={{color: 'white'}}
+        icon={<PersonIcon />}
       />
-      <BottomNavigationAction
-        label="Plan" style={{color: 'white'}}
-        // value="wp"
-        icon={<ListAltIcon />}
-      />
-        <Fab className={classes.fabButton}>
-          <AddIcon />
-        </Fab>
-        {/* <BottomNavigationAction
-          label=""
-          className={classes.addIcon}
-          // value="create"
-          icon={<AddCircleIcon />}
-        /> */}
+      </Link>
+      <Link to='/Create'>
+      <BottomNavigationAction label='Workouts' value='workouts'
+        style={{color: 'white'}} icon={<RowingIcon />}
         />
+        </Link>
+      <Link to='/WorkoutPlanList'>
+      <BottomNavigationAction
+        label="Plan" style={{color: 'white'}} size={50}
+        // value="wp"
+        icon={<ListIcon />}
+      />
+      </Link>
+
+
+        <Link to='/'>
+        <BottomNavigationAction label='Sign Out' value='signout'
+        style={{color: 'white'}} icon={<MeetingRoomIcon />}
+        />
+      </Link>
+    {/* <Link to='/NestList'>
       <BottomNavigationAction
         label="Nests" style={{color: 'white'}}
         // value="nests"
         icon={<BubbleChartIcon />}
       />
+      </Link>
+      <Link to='/FriendList'>
       <BottomNavigationAction
         label="Nesters" style={{color: 'white'}}
         // value="friends"
         icon={<PersonAddIcon />}
       />
+      </Link> */}
     </BottomNavigation>
     </div>
   );
--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,105 +1,105 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Paper from '@material-ui/core/Paper';
-import { withStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import arnold from '../../assets/images/arnold.png'
-import { Link } from 'react-router-dom';
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import Paper from '@material-ui/core/Paper';
+// import { withStyles } from '@material-ui/core/styles';
+// import Grid from '@material-ui/core/Grid';
+// import Avatar from '@material-ui/core/Avatar';
+// import Button from '@material-ui/core/Button';
+// import arnold from '../../assets/images/arnold.png'
+// import { Link } from 'react-router-dom';
 
 
 
-class Profile extends Component {
-    state = {
-            name: '',
-            type: '',
-            date: '',
-            description: '',
-            // need to get exact id for login button
-            // do i have to define this as a variable above?
-            // userid: localStorage.getItem('userLogin'),
-            userId: '',
-            workoutPlanList: [], 
-            nestList: []
-    }
-    componentWillMount() {
-            workoutPlanList.getSome(1)
-                    .then(({ data }) => {
-                            this.setState({ workoutPlanList: data })
-                    })
-    }
+// class Profile extends Component {
+//     state = {
+//             name: '',
+//             type: '',
+//             date: '',
+//             description: '',
+//             // need to get exact id for login button
+//             // do i have to define this as a variable above?
+//             // userid: localStorage.getItem('userLogin'),
+//             userId: '',
+//             workoutPlanList: [], 
+//             nestList: []
+//     }
+//     componentWillMount() {
+//             workoutPlanList.getSome(1)
+//                     .then(({ data }) => {
+//                             this.setState({ workoutPlanList: data })
+//                     })
+//     }
 
 
-    handleGetOccasions = event => {
-            const userId = this.state.userId
-            console.log("Here I am!")
-            workoutPlanList.getAll()
-    }
+//     handleGetOccasions = event => {
+//             const userId = this.state.userId
+//             console.log("Here I am!")
+//             workoutPlanList.getAll()
+//     }
 
-    // handleSelectImage = (type) => {
-    //         switch ({ type }) {
-    //                 case 'birthday':
-    //                         console.log('birthday')
-    //                         this.document.CardMedia.style.backgroundImage = "url('./assets/images/balloons.png"
-    //                         break
-    //                 case 'wedding':
-    //                         console.log('wedding')
-    //                         this.document.CardMedia.style.backgroundImage = "url('./assets/images/wedding.jpg"
-    //                         break
-    //                 case 'valentines':
-    //                         console.log('valentines')
-    //                         document.body.style.backgroundImage = "url('./assets/images/valentines.jpg"
-    //                         break
-    //                 case 'baby':
-    //                         console.log('baby')
-    //                         document.body.style.backgroundImage = "url('./assets/images/balloons.png"
-    //                         break
-    //                 case 'because':
-    //                         console.log('because')
-    //                         document.body.style.backgroundImage = "url('./assets/images/balloons.png"
-    //                         break
-    //         }
-    }
+//     // handleSelectImage = (type) => {
+//     //         switch ({ type }) {
+//     //                 case 'birthday':
+//     //                         console.log('birthday')
+//     //                         this.document.CardMedia.style.backgroundImage = "url('./assets/images/balloons.png"
+//     //                         break
+//     //                 case 'wedding':
+//     //                         console.log('wedding')
+//     //                         this.document.CardMedia.style.backgroundImage = "url('./assets/images/wedding.jpg"
+//     //                         break
+//     //                 case 'valentines':
+//     //                         console.log('valentines')
+//     //                         document.body.style.backgroundImage = "url('./assets/images/valentines.jpg"
+//     //                         break
+//     //                 case 'baby':
+//     //                         console.log('baby')
+//     //                         document.body.style.backgroundImage = "url('./assets/images/balloons.png"
+//     //                         break
+//     //                 case 'because':
+//     //                         console.log('because')
+//     //                         document.body.style.backgroundImage = "url('./assets/images/balloons.png"
+//     //                         break
+//     //         }
+//     }
 
-    handleStorage = ({ id, name, type, date, description }) => {
-            let cardData = { id, name, type, date, description }
-            localStorage.setItem('update', JSON.stringify(cardData))
-            this.setState({ ...this.state, currentCard: cardData, isEditing: true })
-            console.log(cardData)
-        }
+//     handleStorage = ({ id, name, type, date, description }) => {
+//             let cardData = { id, name, type, date, description }
+//             localStorage.setItem('update', JSON.stringify(cardData))
+//             this.setState({ ...this.state, currentCard: cardData, isEditing: true })
+//             console.log(cardData)
+//         }
     
 
-    handleDeleteWorkoutPlan = (id) => {
-            console.log('Here I am!')
-            workoutPlanList.deleteOne(id)
-                    .then(({ data }) => {
-                            this.setState({
-                                workoutPlanList: this.state.workoutPlanList.filter(workoutPlan => workoutPlan.id !== id)
-                            })
-                    })
-    }
+//     handleDeleteWorkoutPlan = (id) => {
+//             console.log('Here I am!')
+//             workoutPlanList.deleteOne(id)
+//                     .then(({ data }) => {
+//                             this.setState({
+//                                 workoutPlanList: this.state.workoutPlanList.filter(workoutPlan => workoutPlan.id !== id)
+//                             })
+//                     })
+//     }
 
 
-    render() {
-            return (
-                    <>
-                            <div>
-                                    {/* <TopNav /> */}
-                                    <ProfileTop />
-                                    {/* <OccList /> */}
-                                    <BottomNav />
-                            </div>
-                            {this.state.isEditing ? <EditForm currentworkoutPlan={this.state.currentCard} /> : null}
-                            <OccCard
-                                    newworkoutplan={this.state.workoutPlanList}
-                                    handleSelectChange={this.state.type}
-                                    handleStorage={this.handleStorage}
-                                    handleDeleteWorkoutPlan={this.handleDeleteWorkoutPlan}
-                            />
-                    </>
-            )
-    }
-}
+//     render() {
+//             return (
+//                     <>
+//                             <div>
+//                                     {/* <TopNav /> */}
+//                                     <ProfileTop />
+//                                     {/* <OccList /> */}
+//                                     <BottomNav />
+//                             </div>
+//                             {this.state.isEditing ? <EditForm currentworkoutPlan={this.state.currentCard} /> : null}
+//                             <OccCard
+//                                     newworkoutplan={this.state.workoutPlanList}
+//                                     handleSelectChange={this.state.type}
+//                                     handleStorage={this.handleStorage}
+//                                     handleDeleteWorkoutPlan={this.handleDeleteWorkoutPlan}
+//                             />
+//                     </>
+//             )
+//     }
+// }
 
-export default Profile
+// export default Profile
