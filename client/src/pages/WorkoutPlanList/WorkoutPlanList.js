fa553adb36a6dd47575f3bf60425d956875cfb1d
g-uh22 <47232802+g-uh22@users.noreply.github.com>
2019-07-25 18:15:15 -0700

    styling workoutplanlist

--- client/src/pages/WorkoutPlanList/WorkoutPlanList.js
+++ client/src/pages/WorkoutPlanList/WorkoutPlanList.js
@@ -4,14 +4,31 @@ import Paper from '@material-ui/core/Paper';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
 import arnold from '../../assets/images/arnold.png'
 import { Link } from 'react-router-dom';
 import axios from 'axios'
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import ProfileTop from '../../components/ProfileTop'
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
 
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
+
+// export default function MediaCard() {
+//   const classes = useStyles();
 
 class WorkoutPlanList extends Component {
     state = {
@@ -28,6 +45,11 @@ class WorkoutPlanList extends Component {
         
     }
 
+        handleViewWorkoutPlan = () => {
+          this.setState(() => ({
+            viewWorkouts: true
+          }))
+        }
    
 
     componentWillMount() {
@@ -47,6 +69,45 @@ class WorkoutPlanList extends Component {
                     })
     }
     
+//     export default function MediaCard() {
+//   const classes = useStyles();
+
+
+render() {
+          if (this.state.viewWorkouts === true) {
+            return <Redirect to='/PlanExercises' />
+          }
+          const { classes } = this.props
+  
+  return (
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="https://image.flaticon.com/icons/png/512/110/110495.png"
+          title="Barbell"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {this.state.name}
+            {this.state.focus}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {this.state.goal}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary" className={classes.button} onClick={this.handleViewWorkoutPlan}>
+          View Workout Plan
+        </Button>
+       <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+}
         
 
 
@@ -60,21 +121,21 @@ class WorkoutPlanList extends Component {
 
 
 
-    render() {
-        // {!viewWorkouts ? 
-            return (
-                    <>
-                    <div>
-                        <h1> {this.state.name} </h1>
-</div>
-                    </>
+//     render() {
+//         // {!viewWorkouts ? 
+//             return (
+//                     <>
+//                     <div>
+//                         <h1> {this.state.name} </h1>
+// </div>
+//                     </>
 
                         
-            )
-    }
-}
+//             )
+//     }
+// }
 
-export default WorkoutPlanList
+export default makeStyles(styles)(WorkoutPlanList)
 //1. test plan route (works on local but is Dean inputting plans?)
 //2. Test axios.get('/plans) to render components
 //Test get workout(id hardcode in id) then render component
